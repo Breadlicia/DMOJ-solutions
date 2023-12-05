99d0baa2e701a74c81d2b1abb8547a99684d7bf4
Bread <86811307+Breadlicia@users.noreply.github.com>
2023-12-05 22:54:28 +0000

    day 1

---
+++ Hello.py
@@ -0,0 +1,66 @@
+# Variables
+
+# string data type
+# first_name = "John"
+# last_name = "Smith"
+# full_name = first_name +" "+ last_name
+# print("Hello "+full_name)
+
+# int data type
+# age = 10
+# age += 1
+# print("your age is: "+str(age))
+
+# float data type
+# height = 250.5
+# print("your height is: "+str(height)+"cm")
+
+# bool data type
+# useful for if statements
+# human = False
+# print("are you a human: "+str(human))
+
+# Multiple assignement
+
+# name = "John"
+# age = 10
+# chad = True
+
+# name, age, chad = "John", 10, True
+# year = month = day = 1
+
+# String methods
+
+# name = "John"
+# print(len(name))
+
+# type casting
+
+# x = 1
+# y = 2.0
+# z = "3"
+
+# y = int(y) 
+# print(y*3)
+
+# User input
+
+# age = int(input("What is your age?: "))
+# age = age + 1
+# print("You are "+str(age)+" years old")
+
+# Math functions
+
+# import math
+
+# pi = 3.14
+
+# print(round(pi))
+
+# String slicing
+
+name = "John Smith"
+
+first_name = name[0]
+
+print(first_name)
