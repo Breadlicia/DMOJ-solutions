cc1dd138a0d2ff39b7816688d31df79a7b22babe
Bread <86811307+Breadlicia@users.noreply.github.com>
2023-12-06 22:44:50 +0000

    p4

---
+++ P4Telemarketers.py
@@ -0,0 +1,15 @@
+firstnumber=int(input())
+secondnumber=int(input())
+thirdnumber=int(input())
+fourthnumber=int(input())
+
+if firstnumber==8 or firstnumber==9:
+    if secondnumber-thirdnumber==0:
+        if fourthnumber==8 or fourthnumber==9:
+            print("ignore")
+        else:
+            print("answer")
+    else:
+        print("answer")
+else:
+    print("answer")
