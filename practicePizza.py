9ceebd9d7a1f55f78dbc5c814661ce4ea78e4fa7
Bread <86811307+Breadlicia@users.noreply.github.com>
2023-12-07 02:38:58 +0000

    pza

---
+++ practicePizza.py
@@ -0,0 +1,13 @@
+start=input().split()
+n=int(start[0])
+slices=int(start[1])
+ovens=int(start[2])
+minutes=int(start[3])
+
+for x in range(n):
+    a #unfinished
+
+print(str(n))
+print(str(slices))
+print(str(ovens))
+print(str(minutes))
